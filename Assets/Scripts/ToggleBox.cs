002695a8cc4f6f51315caa2f09bd98c352a8c8bd
sunflwwer <ghosti007@naver.com>
2025-09-03 17:19:44 +0900

    add particle, togglebridge

--- Assets/Scripts/ToggleBox.cs
+++ Assets/Scripts/ToggleBox.cs
@@ -1,19 +1,73 @@
 using UnityEngine;
+using System.Collections;
 
 public class ToggleBox : MonoBehaviour
 {
-    [SerializeField] float interval = 3f; // 몇 초마다 켜고 끌지
-    private bool isActive = true;
+    [SerializeField] private float onTime = 1f;      // 켜져 있는 시간
+    [SerializeField] private float offTime = 2f;     // 꺼져 있는 시간
+    [SerializeField] private float startDelay = 0f;  // 시작 지연 (박스마다 다르게 설정)
+    [SerializeField] private float hiddenAlpha = 0.1f; // 투명해질 때 알파 값 (0=완전투명, 1=불투명)
+
+    private Renderer[] renderers;
+    private Collider col;
+    private Material[] materials;
+
+    void Awake()
+    {
+        renderers = GetComponentsInChildren<Renderer>();
+        col = GetComponent<Collider>();
+
+        // 머티리얼 배열 저장
+        materials = new Material[renderers.Length];
+        for (int i = 0; i < renderers.Length; i++)
+        {
+            materials[i] = renderers[i].material;
+        }
+    }
 
     void Start()
     {
-        // 3초마다 ToggleActive 함수를 반복 실행
-        InvokeRepeating(nameof(ToggleActive), interval, interval);
+        StartCoroutine(ToggleRoutine());
     }
 
-    void ToggleActive()
+    IEnumerator ToggleRoutine()
     {
-        isActive = !isActive;
-        gameObject.SetActive(isActive);
+        // 지정한 딜레이만큼 기다렸다가 시작
+        yield return new WaitForSeconds(startDelay);
+
+        while (true)
+        {
+            // 켜기 (알파 1, 콜라이더 켜기)
+            SetVisible(true);
+            yield return new WaitForSeconds(onTime);
+
+            // 끄기 (알파 낮추고, 콜라이더 끄기)
+            SetVisible(false);
+            yield return new WaitForSeconds(offTime);
+        }
+    }
+
+    void SetVisible(bool visible)
+    {
+        if (visible)
+        {
+            foreach (var mat in materials)
+            {
+                Color c = mat.color;
+                c.a = 1f;         // 완전 보이게
+                mat.color = c;
+            }
+            if (col != null) col.enabled = true;
+        }
+        else
+        {
+            foreach (var mat in materials)
+            {
+                Color c = mat.color;
+                c.a = hiddenAlpha; // 반투명
+                mat.color = c;
+            }
+            if (col != null) col.enabled = false;
+        }
     }
 }
--- Assets/Scripts/brickMoveController.cs
+++ Assets/Scripts/brickMoveController.cs
@@ -3,25 +3,29 @@ using UnityEngine;
 public class brickMoveController : MonoBehaviour
 {
     public float distance = 1f;
+    private bool stopped = false;
 
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-
-    }
-    //플레이어와 충돌 후 떨어질때 로컬방향 앞으로 이동
     void OnCollisionExit(Collision collision)
     {
-        //bool에서 선택된 로컬방향으로 이동
-        if (collision.gameObject.tag == "Player")
+        if (!stopped && collision.gameObject.CompareTag("Player"))
         {
-         transform.Translate(Vector3.forward * distance);
+            // 앞으로 Raycast 쏴서 막혀 있으면 이동 안함
+            if (!Physics.Raycast(transform.position, transform.forward, distance))
+            {
+                transform.Translate(Vector3.forward * distance);
+            }
+            else
+            {
+                stopped = true;
+            }
+        }
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Platform"))
+        {
+            stopped = true;
         }
     }
 }
